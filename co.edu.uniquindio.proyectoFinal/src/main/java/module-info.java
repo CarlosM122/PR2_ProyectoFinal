2a4790342db7a53add6653989c30aea2eb960739
MariaJAngulo <142365378+MariaJAngulo@users.noreply.github.com>
2024-04-14 23:29:18 -0500

    creacionCrudVenta

--- co.edu.uniquindio.proyectoFinal/src/main/java/module-info.java
+++ co.edu.uniquindio.proyectoFinal/src/main/java/module-info.java
@@ -5,8 +5,9 @@ module co.edu.uniquindio.proyectofinal.fruteriaapp {
 
     opens co.edu.uniquindio.proyectofinal.fruteriaapp to javafx.fxml;
     exports co.edu.uniquindio.proyectofinal.fruteriaapp;
+    opens co.edu.uniquindio.proyectofinal.fruteriaapp.Controller;
     exports co.edu.uniquindio.proyectofinal.fruteriaapp.Controller;
-    opens co.edu.uniquindio.proyectofinal.fruteriaapp.Controller to javafx.fxml;
+
 
     opens co.edu.uniquindio.proyectofinal.fruteriaapp.ViewController;
     exports co.edu.uniquindio.proyectofinal.fruteriaapp.ViewController;
